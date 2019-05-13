2d6783623f4578897bc4372f28c55627f3c43a1f
Eugène CAUSSAT <caussatjerome@users.noreply.github.com>
2019-05-14 01:44:17 +0200

    Update zeronetscript.sh

--- zeronetscript.sh
+++ zeronetscript.sh
@@ -4,19 +4,49 @@ wget https://github.com/HelloZeroNet/ZeroNet/archive/master.tar.gz
 tar xvpfz master.tar.gz
 rm master.tar.gz
 
-#set as service :
-echo [Unit]>/etc/systemd/system/Zeronet.service
-echo Description=ZeroNet Daemon>/etc/systemd/system/Zeronet.service
-echo After=network-online.target>/etc/systemd/system/Zeronet.service
-echo After=tor.service>/etc/systemd/system/Zeronet.service
-echo [Service]>/etc/systemd/system/Zeronet.service
-echo Type=simple>/etc/systemd/system/Zeronet.service
-echo ExecStart=python /etc/ZeroNet-master/zeronet.py>/etc/systemd/system/Zeronet.service
-echo Restart=on-failure>/etc/systemd/system/Zeronet.service
-echo # Configures the time to wait before service is stopped forcefully.>/etc/systemd/system/Zeronet.service
-echo TimeoutStopSec=300>/etc/systemd/system/Zeronet.service
-echo [Install]>/etc/systemd/system/Zeronet.service
-echo WantedBy=multi-user.target>/etc/systemd/system/Zeronet.service
+#set as service method 1:
+echo [Unit]>/etc/systemd/system/zeronet.service
+echo Description=ZeroNet Daemon>/etc/systemd/system/zeronet.service
+echo Wants=tor.service
+echo After=network-online.target network.target tor.service>/etc/systemd/system/zeronet.service
+echo [Service]>/etc/systemd/system/zeronet.service
+echo EnvironmentFile=/etc/ZeroNet-master/zeronet.conf>/etc/systemd/system/zeronet.service
+echo ExecStart=/etc/ZeroNet-master/zeronet.py --ui_host 0.0.0.0>/etc/systemd/system/zeronet.service
+echo Restart=on-failure>/etc/systemd/system/zeronet.service
+echo '# Configures the time to wait before service is stopped forcefully.'>/etc/systemd/system/zeronet.service
+echo User=yourusernamerunningtheservice>/etc/systemd/system/zeronet.service
+echo TimeoutStopSec=30min>/etc/systemd/system/zeronet.service
+echo [Install]>/etc/systemd/system/zeronet.service
+echo WantedBy=multi-user.target>/etc/systemd/system/zeronet.service
+sudo chmod 664 /etc/systemd/system/zeronet.service
+sudo systemctl daemon-reload
+sudo systemctl enable zeronet.service
 
-systemctl enable Zeronet
-systemctl start Zeronet
+#set as service method 2:
+echo #!/bin/sh>/etc/init.d/zeronet.sh
+echo ### BEGIN INIT INFO>/etc/init.d/zeronet.sh
+echo # Provides:          ZeroNet>/etc/init.d/zeronet.sh
+echo # Required-Start:    >/etc/init.d/zeronet.sh
+echo # Required-Stop:     >/etc/init.d/zeronet.sh
+echo # Default-Start:     2 3 4 5>/etc/init.d/zeronet.sh
+echo # Default-Stop:      0 1 6>/etc/init.d/zeronet.sh
+echo # Short-Description: ZeroNet>/etc/init.d/zeronet.sh
+echo # Description:       ZeroNet>/etc/init.d/zeronet.sh
+echo ### END INIT INFO>/etc/init.d/zeronet.sh
+echo # Actions>/etc/init.d/zeronet.sh
+echo case "$1" in>/etc/init.d/zeronet.sh
+echo     start)>/etc/init.d/zeronet.sh
+echo         rm -f /home/pi/ZeroNet-master/data/lock.pid>/etc/init.d/zeronet.sh
+echo         python2 /home/pi/ZeroNet-master/zeronet.py --ui_ip 10.0.0.2>/etc/init.d/zeronet.sh
+echo         ;;>/etc/init.d/zeronet.sh
+echo     stop)>/etc/init.d/zeronet.sh
+echo         # STOP>/etc/init.d/zeronet.sh
+echo         ;;>/etc/init.d/zeronet.sh
+echo     restart)>/etc/init.d/zeronet.sh
+echo         # RESTART>/etc/init.d/zeronet.sh
+echo         ;;>/etc/init.d/zeronet.sh
+echo esac>/etc/init.d/zeronet.sh
+echo exit 0>/etc/init.d/zeronet.sh
+
+sudo chmod 664 /etc/init.d/zeronet.sh
+update-rc.d zeronet defaults
