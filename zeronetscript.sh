028b9fd2077447037324e2a7e31447a35df97f70
Eugène CAUSSAT <caussatjerome@users.noreply.github.com>
2019-05-14 02:55:55 +0200

    Update zeronetscript.sh

--- zeronetscript.sh
+++ zeronetscript.sh
@@ -34,30 +34,5 @@ echo 'exit 0'>>/etc/init.d/zeronet
 chmod 775 /etc/init.d/zeronet
 update-rc.d zeronet defaults
 iptables -A INPUT -p tcp -m tcp --dport 43110 -j ACCEPT
-
-#set as service method 2:
-rem /etc/systemd/system/zeronet.service
-echo '### BEGIN INIT INFO'>/etc/systemd/system/zeronet.service
-echo '# Provides:          zeronet'>>/etc/systemd/system/zeronet.service
-echo '# Default-Start:     2 3 4 5'>>/etc/systemd/system/zeronet.service
-echo '# Default-Stop:      0 1 6'>>/etc/systemd/system/zeronet.service
-echo '# Short-Description: Start daemon at boot time'>>/etc/systemd/system/zeronet.service
-echo '# Description:       Enable service provided by daemon.'>>/etc/systemd/system/zeronet.service
-echo '### END INIT INFO'>>/etc/systemd/system/zeronet.service
-echo [Unit]>>/etc/systemd/system/zeronet.service
-echo Description=ZeroNet Daemon>>/etc/systemd/system/zeronet.service
-echo Wants=tor.service>>/etc/systemd/system/zeronet.service
-echo After=network-online.target network.target tor.service>>/etc/systemd/system/zeronet.service
-echo [Service]>>/etc/systemd/system/zeronet.service
-echo EnvironmentFile=/etc/ZeroNet-master/zeronet.conf>>/etc/systemd/system/zeronet.service
-echo ExecStart=/etc/ZeroNet-master/zeronet.py --ui_host 0.0.0.0:43110>>/etc/systemd/system/zeronet.service
-echo Restart=on-failure>>/etc/systemd/system/zeronet.service
-echo '# Configures the time to wait before service is stopped forcefully.'>>/etc/systemd/system/zeronet.service
-echo User=yourusernamerunningtheservice>>/etc/systemd/system/zeronet.service
-echo TimeoutStopSec=30min>>/etc/systemd/system/zeronet.service
-echo [Install]>>/etc/systemd/system/zeronet.service
-echo WantedBy=multi-user.target>>/etc/systemd/system/zeronet.service
-
-chmod 775 /etc/systemd/system/zeronet.service
-systemctl daemon-reload
-systemctl enable zeronet.service
+iptables-save
+reboot
