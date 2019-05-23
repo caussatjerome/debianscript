4604b43322efcb58d5c7315ab880abca807bbba3
Eugène CAUSSAT <caussatjerome@users.noreply.github.com>
2019-05-23 11:06:59 +0200

    Update freenetscript.sh

--- freenetscript.sh
+++ freenetscript.sh
@@ -1,2 +1,10 @@
 wget 'https://github.com/freenet/fred/releases/download/build01484/new_installer_offline_1484.jar' -O new_installer_offline.jar
+apt-get -y install default-jre default-jdk
 java -jar new_installer_offline.jar -console
+adduser freenet
+mkdir /usr/local/freenet
+mv /Freenet/* /usr/local/freenet/
+ln -s /usr/local/freenet/run.sh /etc/init.d/freenet-new
+update-rc.d freenet-new defaults
+sed -i 's/#RUN_AS_USER=/RUN_AS_USER=freenet/g' /usr/local/freenet/run.sh
+/etc/init.d/freenet-new start
