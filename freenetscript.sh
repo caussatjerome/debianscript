fd7bb830cf4de15518d80460f856ef7b8a985e43
Eugène CAUSSAT <caussatjerome@users.noreply.github.com>
2019-05-23 11:46:40 +0200

    Update freenetscript.sh

--- freenetscript.sh
+++ freenetscript.sh
@@ -1,5 +1,6 @@
 wget 'https://github.com/freenet/fred/releases/download/build01484/new_installer_offline_1484.jar' -O new_installer_offline.jar
-apt-get -y install default-jre default-jdk
+#install prerequire (haveged = secure entropy)
+apt-get -y install default-jre default-jdk haveged
 java -jar new_installer_offline.jar -console
 adduser freenet
 mkdir /usr/local/freenet
